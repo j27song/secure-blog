7af8851630ea3208ea32514722939df8b07afc42
Jack Song <jaekuk.jack.song@gmail.com>
2018-03-09 18:29:45 -0500

    Showing username, email on viewpost

--- viewpost.php
+++ viewpost.php
@@ -1,7 +1,13 @@
 <?php require('includes/config.php'); 
-$stmt = $db->prepare('SELECT pid, ptitle, pcont, pdate FROM blog_post WHERE pid = :pid');
+//$stmt = $db->prepare('SELECT pid, ptitle, pcont, pdate FROM blog_post WHERE pid = :pid');
+
+$stmt = $db->prepare('select blog_post.pid, blog_post.ptitle, blog_post.pdesc, blog_post.pcont, blog_post.pdate, blog_members.username, blog_members.email from blog_post inner join blog_members on blog_post.mid=blog_members.mid where pid = :pid');
+
 $stmt->execute(array(':pid' => $_GET['id']));
 $row = $stmt->fetch();
+
+
+
 //if post does not exists redirect user.
 if($row['pid'] == ''){
 	header('Location: ./');
@@ -29,6 +35,7 @@ if($row['pid'] == ''){
 		<?php	
 			echo '<div>';
 				echo '<h1>'.$row['ptitle'].'</h1>';
+				echo '<p>By:'.$row['username'].' ('.$row['email'].')</p>';
 				echo '<p>Posted on '.date('jS M Y', strtotime($row['pdate'])).'</p>';
 				echo '<p>'.$row['pcont'].'</p>';				
 			echo '</div>';
