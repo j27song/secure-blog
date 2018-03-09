22ec079407b67f2a1e08e4e44e05d27663b6d7c6
josh-mallari <34925826+josh-mallari@users.noreply.github.com>
2018-03-09 16:27:54 -0500

    Update add-post.php

--- admin/add-post.php
+++ admin/add-post.php
@@ -32,6 +32,12 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 	<h2>Add Post</h2>
 
 	<?php
+	$username = $_SESSION['username'];
+
+	$stmt = $db->query("SELECT mid FROM blog_members where username = '$username'");
+	$row = $stmt->fetch();
+	$id = $row['mid'];
+
 	//if form has been submitted process it
 	if(isset($_POST['submit'])){
 		$_POST = array_map( 'stripslashes', $_POST );
@@ -50,12 +56,13 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
 		if(!isset($error)){
 			try {
 				//insert into database
-				$stmt = $db->prepare('INSERT INTO blog_post (ptitle,pdesc,pcont,pdate) VALUES (:ptitle, :pdesc, :pcont, :pdate)') ;
+				$stmt = $db->prepare('INSERT INTO blog_post (ptitle,pdesc,pcont,pdate,mid) VALUES (:ptitle, :pdesc, :pcont, :pdate, :mid)') ;
 				$stmt->execute(array(
 					':ptitle' => $ptitle,
 					':pdesc' => $pdesc,
 					':pcont' => $pcont,
-					':pdate' => date('Y-m-d H:i:s')
+					':pdate' => date('Y-m-d H:i:s'),
+					':mid' => $id
 				));
 				//redirect to index page
 				header('Location: index.php?action=added');
