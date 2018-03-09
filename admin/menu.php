a49c9ce3501505e9994dbeedd39fa1abe24a0010
josh-mallari <34925826+josh-mallari@users.noreply.github.com>
2018-03-08 23:10:09 -0500

    Update menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -2,7 +2,7 @@
 <p>Logged in as <?=$_SESSION['username'];?></p>
 <ul id='adminmenu'>
 	<li><a href='index.php'>Blog</a></li>
-	//<li><a href='users.php'>Users</a></li>
+	<!-- <li><a href='users.php'>Users</a></li> -->
 	<li><a href="../" target="_blank">View Website</a></li>
 	<li><a href='logout.php'>Logout</a></li>
 </ul>
