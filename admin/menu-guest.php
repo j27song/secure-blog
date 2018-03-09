bdf9d114f0b070678f33cdbb81431428212f4da0
josh-mallari <34925826+josh-mallari@users.noreply.github.com>
2018-03-09 13:52:28 -0500

    Update menu-guest.php

--- admin/menu-guest.php
+++ admin/menu-guest.php
@@ -1,7 +1,7 @@
-<h1>Blog</h1>
+<h1>J&J's Blog</h1>
 <p>Welcome Guest!</p>
 <ul id='adminmenu'>
-	<li><a href='index.php'>Blog</a></li>
+	<li><a href='index.php'>Blog Management</a></li>
 	<!-- <li><a href='users.php'>Users</a></li> -->
 	<li><a href="../" target="_blank">View Website</a></li>
 </ul>
