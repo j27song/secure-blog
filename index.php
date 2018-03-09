bee602f6e685c48be74a68ee917b7c287de7f485
Jack Song <jaekuk.jack.song@gmail.com>
2018-03-09 18:17:38 -0500

    Showing username, email on post

--- index.php
+++ index.php
@@ -27,16 +27,19 @@ if($user->is_logged_in()){ header('Location: index-logged.php'); }
 
 		<?php
 			try {
-				$stmt = $db->query('SELECT pid, ptitle, pdesc, pdate FROM blog_post ORDER BY pid DESC');
+				//$stmt = $db->query('SELECT pid, ptitle, pdesc, pdate FROM blog_post ORDER BY pid DESC');
+$stmt = $db->query('select blog_post.pid, blog_post.ptitle, blog_post.pdesc, blog_post.pcont, blog_post.pdate, blog_members.username, blog_members.email from blog_post inner join blog_members on blog_post.mid=blog_members.mid ORDER BY pid DESC');
 				while($row = $stmt->fetch()){
 					
 					echo '<div>';
 						echo '<h1><a href="viewpost.php?id='.$row['pid'].'">'.$row['ptitle'].'</a></h1>';
 						echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['pdate'])).'</p>';
+						echo '<p>By:'.$row['username'].' ('.$row['email'].')</p>';
 						echo '<p>'.$row['pdesc'].'</p>';				
 						echo '<p><a href="viewpost.php?id='.$row['pid'].'">Read More</a></p>';				
 					echo '</div>';
 				}
+
 			} catch(PDOException $e) {
 			    echo $e->getMessage();
 			}
